b6ea4603947946a18e2cfc0009f3dc1044946e5a
Ashley <thy.ashley@gmail.com>
2021-02-28 16:49:25 +0800

    Added procedural programming for getting unique item in list.

--- Hello World/Tutorial/ProceduralProgramming/Program.cs
+++ Hello World/Tutorial/ProceduralProgramming/Program.cs
@@ -1,26 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProceduralProgramming
 {
     class Program
     {
+        public static List<int> AddNumberToArray()
+        {
+            var numbers = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("Enter Number or Quit to exit");
+                var response = Console.ReadLine();
+                if (response.Trim().ToLower() == "quit")
+                {
+                    break;
+                }
+                try
+                {
+                    numbers.Add(int.Parse(response));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Number");
+                }
+            }
+            return numbers;
+        }
+        public static List<int> GetUniqueList(List<int> numbers)
+        {
+            var uniqueList = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniqueList.Contains(number))
+                {
+                    uniqueList.Add(number);
+                }
+            }
+            return uniqueList;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name:");
-            var name = Console.ReadLine();
+            var numbers = AddNumberToArray();
+            var uniqueList = GetUniqueList(numbers);
 
-            Console.WriteLine(ReverseName(name));
+            Console.WriteLine("Unique Numbers: ");
+            foreach(var item in uniqueList)
+            {
+                Console.Write(item + ", ");
+            }
         }
 
-        public static string ReverseName(string name)
-        {
-            var array = name.ToCharArray();
-            return String.Join("", array.Reverse());
-        }
+
         
     }
 }
