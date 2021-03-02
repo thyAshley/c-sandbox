1f19bb9c90769b7e60c3f6b40b9b1952f0cc59e1
Ashley <thy.ashley@gmail.com>
2021-03-02 16:59:06 +0800

    Compositions

---
+++ Intermediate/Composition/DbMigrator.cs
@@ -0,0 +1,17 @@
+﻿namespace Composition
+{
+    public class DbMigrator
+    {
+        private readonly Logger _logger;
+
+        public DbMigrator(Logger logger)
+         {
+            this._logger = logger;
+            }
+
+        public void Migrate()
+        {
+            _logger.Log("Migrating...");
+        }
+    }
+}
---
+++ Intermediate/Composition/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    public class Tester
+    {
+
+    }
+
+    public class Installer
+    {
+        private readonly Logger _logger;
+        private readonly Tester _test;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public Installer (Tester test)
+        {
+            _test = test;
+        }
+        public void Install()
+        {
+            _logger.Log("We are installing the application..");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(new Logger());
+
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
