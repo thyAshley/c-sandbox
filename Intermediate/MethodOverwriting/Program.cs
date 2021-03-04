eae0b7ff227027565022e87ec73a63e2e18277a1
Ashley <thy.ashley@gmail.com>
2021-03-04 18:41:27 +0800

    added single linked list function

---
+++ Intermediate/MethodOverwriting/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverwriting
+{
+    public class Canvas
+    {
+        public static void DrawShape(List<Shape> shapes)
+        {
+            foreach(var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+    public class Position
+    {
+
+    }
+
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+    }
+
+    public class Circle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw Circle..");
+        }
+    }
+
+    public class Rectangle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapeList = new List<Shape>();
+            shapeList.Add(new Circle());
+            shapeList.Add(new Rectangle());
+            shapeList.Add(new Circle());
+            Canvas.DrawShape(shapeList);
+
+
+        }
+    }
+}
---
+++ Intermediate/SingleLinkedList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    public class Node
+    {
+        public Node next;
+        public string val;
+
+        public Node(string value)
+        {
+            this.val = value;
+            this.next = null;
+        }
+
+    }
+
+    public class LinkedList
+    {
+        public Node tail;
+        public Node head;
+        public int length;
+
+        public LinkedList()
+        {
+            this.tail = null;
+            this.head = null;
+            this.length = 0;
+        }
+
+        public void push(string val)
+        {
+            var node = new Node(val);
+            if (this.length == 0)
+            {
+                this.head = node;
+                this.tail = node;
+            } else
+            {
+                this.tail.next = node;
+                this.tail = node;
+            }
+            this.length += 1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList();
+            list.push("Hey");
+            list.push("There");
+            Console.WriteLine(list.head.next.val);
+        }
+    }
+}
